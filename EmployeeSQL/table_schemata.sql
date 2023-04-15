7e0a70d04f9e4a00705c2ad132044ab0d65bb063
JoshuaMayo96 <joshuawademayo@gmail.com>
2023-04-15 12:00:56 -0600

    initial commit

---
+++ EmployeeSQL/table_schemata.sql
@@ -0,0 +1,45 @@
+CREATE TABLE titles (
+	title_id VARCHAR(5) PRIMARY KEY,
+	title VARCHAR(40)
+);
+
+
+CREATE TABLE employees (
+	emp_no INT PRIMARY KEY NOT NULL,
+	emp_title_id VARCHAR(5) NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR(35) NOT NULL,
+	last_name VARCHAR(35) NOT NULL,
+	sex VARCHAR(1) NOT NULL,
+	hire_date DATE NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES titles (title_id)
+);
+
+
+CREATE TABLE departments (
+	dept_no VARCHAR(8) PRIMARY KEY NOT NULL,
+	dept_name VARCHAR(40) NOT NULL
+);
+
+
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(8) NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
+);
+
+
+CREATE TABLE salaries (
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
+
+
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(8) NOT NULL,
+	emp_no INT NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
+);
